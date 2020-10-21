3e90f13d78b8762904857c2bf09ceee3506ce465
Dustin Kendall <dustin.kendall@customcrimp.com>
2020-10-20 23:57:19 -0400

    Added main activity with one button

---
+++ app/src/main/java/com/dustinkendall/templating/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dustinkendall.templating
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Dustin Kendall on 10/20/20.
+ */
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
